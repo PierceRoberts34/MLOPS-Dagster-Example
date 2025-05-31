7c32efc5afa51a8896411f340e31e85a24b17de5
Pierce Roberts <134012559+PierceRoberts34@users.noreply.github.com>
2025-05-31 19:56:06 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,11 +6,3 @@ variable "region" {
   description = "GCP Region"
   type        = string
 }
-variable "zone" {
-  description = "GCP Zone"
-  type        = string
-}
-variable "service_account_email" {
-  description = "Service Account Email"
-  type        = string
-}
