52d140c94fb63a39536a571e83c0843b45449b18
Pierce Roberts <134012559+PierceRoberts34@users.noreply.github.com>
2025-05-31 16:58:58 -0400

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+variable "region" {
+  description = "GCP Region"
+  type        = string
+}
+variable "zone" {
+  description = "GCP Zone"
+  type        = string
+}
+variable "service_account_email" {
+  description = "Service Account Email"
+  type        = string
+}
