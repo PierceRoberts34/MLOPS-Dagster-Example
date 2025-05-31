092a4984936acd53b65b5bcefa9e891c7c2d6246
Pierce Roberts <134012559+PierceRoberts34@users.noreply.github.com>
2025-05-31 17:14:51 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,12 +8,12 @@ provider "google" {
 # Notebook Environment
 resource "google_notebooks_environment" "environment" {
   name = "notebooks-environment"
-  location = "us-west1-a"  
+  location = "us-east1"  
 }
 
 # Tensorboard for Monitoring
 resource "google_vertex_ai_tensorboard" "default" {
   display_name = "vertex-ai-tensorboard-sample-name"
-  region       = "us-central1"
+  region       = "us-east1"
 }
 
